d1a79b67c3775d0412197677733e03b56f633e88
Majkoli <michaelbechaalani1@gmail.com>
2022-12-23 16:59:37 +0100

    Added a new file

---
+++ git.py
@@ -0,0 +1,32 @@
+# == likamed
+# != inte likamed
+# = assign value
+# float (1.0)
+# int (1)
+# bool (True - False)
+# str ("1")
+# if
+# elif
+# else
+# lista []
+# while
+# is
+# int
+# input
+# print
+
+
+
+
+#First = float(input("First: "))
+#Second = float(input("Second: "))
+#Sum = First + Second
+#print("Sum: " + str(Sum)
+
+
+#password = (input("Write Password: "))
+#if password == ("HEJ"):
+#    print("Correct")
+#else:
+#    print("Wrong")
+
